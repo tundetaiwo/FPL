ae0fa475fe9758331579d235de0dd3deb7766cbc
Tunde Taiwo <dantaiwo96@googlemail.com>
2023-04-23 13:23:28 +0100

    add: included definitions of certain league ids

---
+++ Definitions.py
@@ -0,0 +1,13 @@
+import os
+
+SOURCE_DIR = os.path.dirname(__file__)
+
+SUBWAY_LEAGUE = {
+    "Tunde": 7780837,
+    "Gibbo": 4569130,
+    "Mark": 5147964,
+    "Joe": 4854632,
+    "Chidi": 7215032,
+    "Penny": 1092883,
+    "James": 5924210,
+}
