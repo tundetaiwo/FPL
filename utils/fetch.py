09c91a8e39bf9e2b947e0bfc7a7469d36583a328
Tunde Taiwo <u1505814@warwick.ac.uk>
2023-05-31 22:49:16 +0100

    feat: added generic function to fetch requests

---
+++ utils/fetch.py
@@ -0,0 +1,40 @@
+# %%
+import asyncio
+import json
+import ssl
+from typing import Union
+
+import certifi
+import requests
+from aiohttp import ClientSession
+
+# %%
+# -- Define Types -- #
+JSON = int | str | float | bool | None
+JSONObject = dict[str, JSON]
+
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+
+def fetch_request(url: str) -> JSONObject:
+    """
+    Function to fetch and load JSON from url
+
+    Parameters
+    ----------
+    `url (str)`: url to send request
+    """
+    repsonse = requests.get(url)
+    return repsonse.json()
+
+
+async def fetch_request_async(url: str, session: ClientSession) -> JSONObject:
+    """
+    Coroutine to fetch request from url
+
+    Parameters
+    ----------
+    `url (str)`: url to send request
+    `session (ClientSession)`: open aiohttp ClientSession
+    """
+    return await session.get(url, ssl=ssl_context)
