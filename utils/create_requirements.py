eb83f4a38d2083927b7f67652fd9534fe1573cd2
Tunde Taiwo <u1505814@warwick.ac.uk>
2023-06-01 00:08:47 +0100

    refactor: changed write_names to os_names

--- utils/create_requirements.py
+++ utils/create_requirements.py
@@ -8,14 +8,14 @@ from subprocess import PIPE, Popen
 from typing import List
 
 
-def create_requirements(folder: str, write_names: List[str] = None) -> None:
+def create_requirements(folder: str, os_names: List[str] = None) -> None:
     """
     Function to write requirements file for both windows and unix based system
 
     Parameters
     ----------
-    `str` folder: locationn of directory to write requirements files
-    `List[str]` write_names: TODO
+    `str` folder: location of directory to write requirements files
+    `List[str]` os_names: names of operating systems, if None then set to ["win", "unix"]
 
     Returns
     -------
@@ -25,8 +25,8 @@ def create_requirements(folder: str, write_names: List[str] = None) -> None:
     if not os.path.isdir(folder):
         raise ValueError(f"folder argument: '{folder}' is not a directory.")
 
-    if write_names is None:
-        write_names = ["win", "unix"]
+    if os_names is None:
+        os_names = ["win", "unix"]
     os_type = platform.system().lower()
     if os_type in ["linux", "darwin"]:
         message = "python3 -m pip freeze"
@@ -51,7 +51,7 @@ def create_requirements(folder: str, write_names: List[str] = None) -> None:
         requirements_file_win = "\n".join(package_list)
 
     for os_type, requirements_file in zip(
-        write_names, [requirements_file_win, requirements_file_unix]
+        os_names, [requirements_file_win, requirements_file_unix]
     ):
         with open(f"{folder}/requirements_{os_type}.txt", "w") as file:
             print(f"{folder}/requirements_{os_type}")
