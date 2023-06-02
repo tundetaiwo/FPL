e15dd3b05a74c841a3e2ed69b7c18fa0ff32ebf6
Tunde Taiwo (Darwin) <dantaiwo96@gmail.com>
2023-06-02 23:40:42 +0100

    tests: moved out of FPL

--- tests/utils/test_create_requirements.py
+++ tests/utils/test_create_requirements.py
@@ -4,7 +4,7 @@ import subprocess
 import pytest
 
 # from ...Definitions import SOURCE_DIR
-from ...utils import create_requirements
+from FPL.utils import create_requirements
 
 
 class TestCreateRequirements:
