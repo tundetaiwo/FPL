d3c8bfe23409bcbee95b4494e293cd0d4b8b3e86
Tunde Taiwo (Unix) <dantawio96@googlemail.com>
2023-07-13 00:48:32 +0100

    feat: adding initial stages for getting top n users

--- FPL/src/users.py
+++ FPL/src/users.py
@@ -1,5 +1,4 @@
 import asyncio
-import json
 from typing import List
 
 from aiohttp import ClientSession
@@ -14,7 +13,7 @@ async def _get_users_async(ids: List[int]):
 
     Parameters
     ----------
-    `id (int)`:
+    `ids (List[int])`:
 
     Return
     ------
@@ -29,4 +28,38 @@ async def _get_users_async(ids: List[int]):
 
 
 def get_users_async(ids: List[int]):
+    """
+    Wrapper function to asynchronously call _get_users_async
+    """
+    # TODO: return a dataframe somehow
     return asyncio.run(_get_users_async(ids))
+
+
+async def _get_top_users_id(n) -> List[int]:
+    """
+    TODO
+    """
+    pages = (n // 50) + 1
+    urls = [_get_api_url("standings", page) for page in pages]
+    async with ClientSession() as session:
+        tasks = [fetch_request_async(url, session) for url in urls]
+        data = await asyncio.gather(*tasks)
+
+    # TODO: turn data into a list of ids
+    # TODO: return first n in list of ids
+
+
+def get_top_users_id(n: int = 50) -> List[int]:
+    """
+    Wrapper function to asynchronously call _get_top_users_id
+
+    Parameters
+    ----------
+    `n` (int): top n users to retrieve
+
+    Return
+    ------
+    `list`: list of ids of top n users in ascending order
+
+    """
+    data = asyncio.run(_get_top_users_id(n))
