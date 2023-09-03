93f36c5c595fa6f149278516489ac3e6f58a092b
Tunde Taiwo (Unix) <dantawio96@googlemail.com>
2023-09-04 00:28:43 +0100

    refactor: get top user id now returns a list of top user ids

--- FPL/src/users.py
+++ FPL/src/users.py
@@ -7,7 +7,7 @@ from FPL.utils._get_api_url import _get_api_url
 from FPL.utils.fetch import fetch_request_async
 
 
-async def _get_users_async(ids: List[int]):
+async def _get_users_async(ids: List[int], gameweek: int):
     """
     Function to asynchronously retrieve user information
 
@@ -15,38 +15,39 @@ async def _get_users_async(ids: List[int]):
     ----------
     `ids (List[int])`:
 
+    `gameweek (int)`: gameweek up to retrieve manager team information for
+
     Return
     ------
     `user information json`:
 
     """
-    urls = [_get_api_url("entry", id) for id in ids]
+    urls = [_get_api_url("picks", id, gameweek) for id in ids]
     async with ClientSession() as session:
         tasks = [fetch_request_async(url, session) for url in urls]
         data = await asyncio.gather(*tasks)
     return data
 
 
-def get_users_async(ids: List[int]):
+def get_users_async(ids: List[int], gameweek: int):
     """
     Wrapper function to asynchronously call _get_users_async
     """
     # TODO: return a dataframe somehow
-    return asyncio.run(_get_users_async(ids))
+    return asyncio.run(_get_users_async(ids, gameweek=gameweek))
 
 
 async def _get_top_users_id(n) -> List[int]:
     """
     TODO
     """
-    pages = (n // 50) + 1
+    pages = range((n // 50) + 2)
     urls = [_get_api_url("standings", page) for page in pages]
     async with ClientSession() as session:
         tasks = [fetch_request_async(url, session) for url in urls]
         data = await asyncio.gather(*tasks)
 
-    # TODO: turn data into a list of ids
-    # TODO: return first n in list of ids
+    return data
 
 
 def get_top_users_id(n: int = 50) -> List[int]:
@@ -62,6 +63,12 @@ def get_top_users_id(n: int = 50) -> List[int]:
     `list`: list of ids of top n users in ascending order
 
     """
-    data = asyncio.run(_get_top_users_id(n))
+    list_of_pages = asyncio.run(_get_top_users_id(n))
+    top_ids = []
+    # start from 1 as first page is always empty
+    for page in list_of_pages[1:]:
+        # Happy to make the assumption there will always be 50 players on a page
+        for player in range(50):
+            top_ids.append(page.get("standings")["results"][player]["id"])
 
-    return data
+    return top_ids[:n]
