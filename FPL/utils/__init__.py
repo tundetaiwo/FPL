7d196b091a1611e45956320058c79681eff441ad
Tunde Taiwo (Unix) <dantawio96@googlemail.com>
2023-09-06 23:50:36 +0100

    add: new functions to src and utils init files

--- FPL/utils/__init__.py
+++ FPL/utils/__init__.py
@@ -1,3 +1,4 @@
-# from ._get_api_url import _get_api_url
-from .definitions import SOURCE_DIR
+from ._get_api_url import _get_api_url
+from .definitions import POS_DICT, SOURCE_DIR
 from .fetch import fetch_request, fetch_request_async
+from .helpers import get_current_gw
