ab8780cea09b3c2cf4d6fd29b1af3922938bcb99
Tunde Taiwo (Unix) <dantaiwo96@gmail.com>
2023-09-11 10:35:22 +0100

    refactor: renamed functions

--- FPL/src/__init__.py
+++ FPL/src/__init__.py
@@ -1,3 +1,3 @@
 from .players import basic_player_df, get_player_id_dict, get_player_info
 from .teams import get_team_id_dict
-from .users import get_league_data, get_manager_leagues_id, get_top_users_id, get_users
+from .users import get_league_data, get_user_leagues_id, get_users, get_users_id
