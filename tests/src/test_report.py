a3f7d3e5a8374a21519a053b40a60238369f101b
Tunde Taiwo (Unix) <dantaiwo96@gmail.com>
2023-09-13 21:31:21 +0100

    restructure: test_players now test_reports

--- tests/src/test_report.py
+++ tests/src/test_report.py
@@ -1,23 +1,26 @@
 import pytest
 
+from FPL.src import FPLReport
 from FPL.src.players import get_player_info
 
 
 class TestGetPlayerInfo:
+    rpt = FPLReport()
+
     def test_get_player_info_bad_name(self):
         with pytest.raises(ValueError):
-            get_player_info(["False Name"])
+            self.rpt.generate_player_analysis(["False Name"])
 
     def test_get_player_info_bad_id(self):
         with pytest.raises(
             ValueError,
             match="IDs in list do not lie within id dictionary, please consult with get_player_id_dict method.",
         ):
-            get_player_info([12_123])
+            self.rpt.generate_player_analysis([12_123])
 
     def test_get_player_info_bad_mix(self):
         with pytest.raises(
             ValueError,
             match="IDs must be either all strings or all integers.",
         ):
-            get_player_info([121, "Kevin Debruyne"])
+            self.rpt.generate_player_analysis([121, "Kevin Debruyne"])
