7513ceb8615f16d778002045874a7042c4e016a0
Tunde Taiwo (Unix) <dantaiwo96@gmail.com>
2023-09-13 21:40:07 +0100

    refactor: updated main to run Report.py

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-from FPL.src import FPLReport
 import importlib
+import pickle
+from pprint import pprint
 
-class 
-rpt = FPLReport 
+from FPL.src import FPLReport
+
+if __name__ == "__main__":
+    # Tunde user id
+    rpt = FPLReport()
+
+    # rpt.generate_leagues(tunde_id)
+
+    # pprint(data)
+
+    tunde_id = 5770588
+    # rpt.generate_player_analysis([10, 13, 131, 99])
+    # pprint(rpt.recent_df.columns)
+    # pprint(rpt.player_analysis_list)
+    # pprint(rpt.recent_df["round"])
+    # pprint(rpt.upcoming_fixtures_df)
+    # rpt.generate_leagues(tunde_id)
+    
+    
+
+    rpt.full_report(top_n=500, user_id=tunde_id)
+    rpt.run(debug=True, open_window=False)
