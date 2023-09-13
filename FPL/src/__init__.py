f552f84a1c5d66565cbb4cbbb0e3b69e62e6c687
Tunde Taiwo (Unix) <dantaiwo96@gmail.com>
2023-09-13 21:32:23 +0100

    refactor: added FPLReport to src/init file

--- FPL/src/__init__.py
+++ FPL/src/__init__.py
@@ -1,3 +1,4 @@
 from .players import basic_player_df, get_player_id_dict, get_player_info
 from .teams import get_team_id_dict
 from .users import get_league_data, get_user_leagues_id, get_users, get_users_id
+from .Report import FPLReport
