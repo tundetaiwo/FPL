229495e92c0f30cb08f33cb9bd8ec0aba410d8ec
tundetaiwo <dantaiwo96@gmail.com>
2023-09-24 22:41:30 +0100

    fix: caching tests

--- tests/utils/test_caching.py
+++ tests/utils/test_caching.py
@@ -1,8 +1,13 @@
 import os
+import shutil
+
 import numpy as np
 import pandas as pd
-from FPL.utils.caching import clear_dir_cache, dir_cache
 import pytest
+from pandas.testing import assert_frame_equal
+
+from FPL.utils.caching import clear_dir_cache, dir_cache
+
 
 @pytest.fixture
 def data():
@@ -15,15 +20,16 @@ def data():
             "x8": np.random.randint(2, 1000, n),
         }
     )
- 
+
+
 @pytest.fixture(scope="function")
 def cache_dir():
     folder = "./.cache_folder_test"
     os.makedirs(folder, exist_ok=True)
     return folder
 
-class TestCaching:
 
+class TestCaching:
     def test_clear_dir_cache(self, cache_dir):
         clear_dir_cache(cache_dir=cache_dir)
         assert os.listdir(cache_dir) == []
@@ -33,13 +39,10 @@ class TestCaching:
         @dir_cache(cache_dir=cache_dir)
         def f():
             return data
+
         # run twice one to cache then to read in
         result = f()
         assert os.path.exists(f"{cache_dir}/")
         result_cached = f()
-        assert result_cached == result
-
-
-
-
-            
+        assert_frame_equal(result_cached, result)
+        shutil.rmtree(cache_dir)
