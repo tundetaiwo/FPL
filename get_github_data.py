a88aba95b27dd919649b2216d51e42894c8f8b80
tundetaiwo <dantaiwo96@gmail.com>
2023-11-29 22:56:10 +0000

    rename: changed get_github_data to bash script

--- get_github_data.py
+++
@@ -1,4 +0,0 @@
-# %%
-import os
-
-os.system(f"git clone https://github.com/vaastav/Fantasy-Premier-League data/github")
---
+++ get_github_data.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/vaastav/Fantasy-Premier-League github_data
+
+mv github_data/data data
+
+rm -rf github_data
