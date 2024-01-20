5855f3454026c248b1533b3c641835c01e4e4b32
tundetaiwo <dantaiwo96@gmail.com>
2024-01-20 09:57:28 +0000

    refactor: standardise main

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-import importlib
-import pickle
-from pprint import pprint
+# %%
 
-from FPL.src import FPLReport
+from FPL.src import FPLReport, get_user_leagues_id
+
+# from FPL.utils.Manager import Manager
 
 if __name__ == "__main__":
-    # Tunde user id
+
+    refresh = 60
+    tunde_id = 5770588
+
     rpt = FPLReport()
 
-    # rpt.generate_leagues(tunde_id)
+    rpt.full_report(tunde_id, 1_000)
 
-    # pprint(data)
-
-    tunde_id = 5770588
-    # rpt.generate_player_analysis([10, 13, 131, 99])
-    # pprint(rpt.recent_df.columns)
-    # pprint(rpt.player_analysis_list)
-    # pprint(rpt.recent_df["round"])
-    # pprint(rpt.upcoming_fixtures_df)
-    # rpt.generate_leagues(tunde_id)
-    
-    
-
-    rpt.full_report(top_n=500, user_id=tunde_id)
-    rpt.run(debug=True, open_window=False)
+    local_host = {"host": "127.0.0.1", "port": 8080}
+    rpt.run(**local_host)
